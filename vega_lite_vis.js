2d5410db6cbede324d3cc41b48a65b54a3e2c11b
sin010319 <76148563+sin010319@users.noreply.github.com>
2022-10-17 23:48:30 +1100

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,42 @@
+var vg_1 = "map.json";
+
+vegaEmbed("#map", vg_1).then(function(result){
+    // access tej vega view instance
+
+
+}).catch(console.error);
+
+var vg_2 = "topartist2021.json";
+
+vegaEmbed("#donut", vg_2).then(function(result){
+    // access tej vega view instance
+
+
+}).catch(console.error);
+
+
+var vg_3 = "topsongs2021.json";
+
+vegaEmbed("#bar", vg_3).then(function(result){
+    // access tej vega view instance
+
+
+}).catch(console.error);
+
+var vg_4 = "oliviarodrigotopsong.json";
+
+vegaEmbed("#pie", vg_4).then(function(result){
+    // access tej vega view instance
+
+
+}).catch(console.error);
+
+var vg_5 = "oliviarodrigoaus.json";
+
+vegaEmbed("#bubble", vg_5).then(function(result){
+    // access tej vega view instance
+
+
+}).catch(console.error);
+
+
